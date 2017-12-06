6f3726ac95075fd6f646f8a35e772e5862cc0771
DenR <den.ran@tutanota.com>
2017-12-06 13:23:47 -0500

    the change -1

---
+++ addition/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace theadd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Result :");
+            Method m = new Method();
+            int a = m.Add(8, 12);
+            Console.WriteLine(a);
+            Console.Read();
+        }
+    }
+    class Method
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
